277c8651e926967b7747629bd768c0a854e16298
freqmc <nickit.birukoff@yandex.ru>
2026-09-09 23:48:34 +0300

    .

--- src/main/java/org/example/Circle.java
+++ src/main/java/org/example/Circle.java
@@ -1,23 +1,15 @@
 package org.example;
-import java.util.Scanner;
 import java.lang.Math;
 
-public class Circle{
-    double x,y,d;
-    Scanner scanner = new Scanner(System.in);
-    public Circle(double x, double y, double d){
-        this.x = x;
-        this.y = y;
-        this.d = d;
+public class Circle extends Figure{
+    public Circle(double X, double Y, double R){
+        setX(X);
+        setY(Y);
+        setR(R);
     }
 
-    public Circle(){
-        x = scanner.nextDouble();
-        y = scanner.nextDouble();
-        d = scanner.nextDouble();
-    }
-
-    public double findArea(double d){
-        return Math.PI * Math.pow(d/2, 2);
+    @Override
+    public double find_area(){
+        return Math.PI * Math.pow(getR(), 2);
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,26 @@
 package org.example;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        Circle circle1 = new Circle(1.5, 1.5, 6.25);
-        Circle circle2 = new Circle();
-        System.out.println(circle1.findArea(circle1.d));
-        System.out.println(circle2.findArea(circle2.d));
+        Scanner scanner = new Scanner(System.in);
+        Figure figs = new Figure();
+        System.out.println("введите количество фигур");
+        int n = scanner.nextInt();
+        ArrayList<Figure> shapes = new ArrayList<>();
+
+        Random rand = new Random();
+        for (int i = 0; i < n; i++){
+            int fig_choice = rand.nextInt(2);
+            double num = rand.nextDouble(0, 10);
+            if (fig_choice == 0){
+                shapes.add(new Circle(num, num, num));
+            } else {
+                shapes.add(new Rectangle(num, num));
+            }
+        }
+        figs.shape_stat(shapes);
     }
 }
