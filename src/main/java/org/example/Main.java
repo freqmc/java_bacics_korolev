ba66cf63a6b89b2953160fd0919e15a8cf6159ee
freq <99544223+freqmc@users.noreply.github.com>
2026-09-09 21:34:56 +0300

    Delete src/main/java/org/example directory

--- src/main/java/org/example/Main.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-public class Main {
-    public static void main(String[] args) {
-        Circle circle1 = new Circle(1.5, 1.5, 6.25);
-        Circle circle2 = new Circle();
-        System.out.println(circle1.findArea(circle1.d));
-        System.out.println(circle2.findArea(circle2.d));
-    }
-}
