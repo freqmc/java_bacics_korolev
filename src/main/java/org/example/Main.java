f97c8e5a3a9159ae55f954a4eaac862e34fa68f2
freqmc <nickit.birukoff@yandex.ru>
2026-09-10 00:25:07 +0300

    .

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,13 +1,11 @@
 package org.example;
 import java.util.ArrayList;
 import java.util.Scanner;
-import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         ArrayBubbleSort ABS = new ArrayBubbleSort();
-        Random rand = new Random();
         ArrayList<Integer> arr = new ArrayList<>();
         int n = scanner.nextInt();
         ABS.arr_fill(arr, n);
